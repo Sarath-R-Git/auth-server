2de300702b1dbf02ac6077029c0673e413297303
Sarath R <sarath.r@cscs.io>
2025-02-20 23:21:44 +0530

    Added necessary changes for the user APIs

--- src/main/java/authserver/mapper/decorators/UserDecorator.java
+++ src/main/java/authserver/mapper/decorators/UserDecorator.java
@@ -5,21 +5,20 @@ import authserver.mapper.interfaces.UserMapper;
 import authserver.model.UserEntity;
 import org.mapstruct.AfterMapping;
 import org.mapstruct.MappingTarget;
-import org.springframework.context.annotation.Primary;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
-@Primary
-public class UserDecorator implements UserMapper {
+public abstract class UserDecorator implements UserMapper {
 
-    private final UserMapper delegate;
+    protected UserMapper delegate;
 
-     public UserDecorator(UserMapper delegate){
+    @Autowired
+    public void setDelegate(UserMapper delegate) {
         this.delegate = delegate;
     }
-
 
     @Override
     public UserEntity AtoB(User user) {
@@ -38,29 +37,28 @@ public class UserDecorator implements UserMapper {
     @Override
     public List<UserEntity> ListOfAtoB(List<User> users) {
         List<UserEntity> entityList = delegate.ListOfAtoB(users);
-
-        for(int i = 0; i < entityList.size(); ++i) {
+        for (int i = 0; i < entityList.size(); i++) {
             afterMappingToB(entityList.get(i), users.get(i));
         }
-
         return entityList;
     }
 
     @Override
     public List<User> ListOfBtoA(List<UserEntity> usersEntity) {
-
         List<User> dtoList = delegate.ListOfBtoA(usersEntity);
-
-        for(int i = 0; i < dtoList.size(); ++i) {
+        for (int i = 0; i < dtoList.size(); i++) {
             afterMappingToA(dtoList.get(i), usersEntity.get(i));
         }
-
         return dtoList;
     }
+
     @AfterMapping
     protected void afterMappingToA(@MappingTarget User dto, UserEntity entity) {
+
     }
+
     @AfterMapping
     protected void afterMappingToB(@MappingTarget UserEntity entity, User dto) {
+
     }
 }
